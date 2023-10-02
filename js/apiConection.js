58998998c1e85b6183b58f208f9ec78f5d6f8b75
Vinicius Pereira Macedo <83282287+viniciuspmacedo@users.noreply.github.com>
2023-10-02 14:37:54 -0300

    Update apiConection.js

--- js/apiConection.js
+++ js/apiConection.js
@@ -1,17 +1,17 @@
 async function apiProducts() {
-    const response = await fetch("http://localhost:3000/products");
+    const response = await fetch("https://meteora-json-server-rho.vercel.app/products");
     const productsList = await response.json();
     return productsList;
 }
 
 async function apiCategories(){
-    const apiResponse = await fetch("http://localhost:3000/categories");
+    const apiResponse = await fetch("https://meteora-json-server-rho.vercel.app/categories");
     const categoriesList = await apiResponse.json();
     return categoriesList;
 }
 
 async function searchInProducts(searchedTerm) {
-    const apiResponse = await fetch(`http://localhost:3000/products/?q=${searchedTerm}`);
+    const apiResponse = await fetch(`https://meteora-json-server-rho.vercel.app/products/?q=${searchedTerm}`);
     const result = await apiResponse.json();
     return result;
 }
